faac1d09344f49c935168dbd540e665d57fd336d
Nelson Fernandez <nifersz@icloud.com>
2021-04-22 15:53:29 -0500

    Smart contract is deployed on the Ethereum RINKEBY test network

--- project-6/truffle-config.js
+++ project-6/truffle-config.js
@@ -1,3 +1,7 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const infuraKey = "2ac87f0bdc004deab755bb718161f99b";
+const mnemonic = "USE YOUR SPECIAL WORDS RIGHT HERE";
+
 module.exports = {
   networks: {
     development: {
@@ -6,6 +10,14 @@ module.exports = {
       network_id: "*", // Match any network id
       websockets: true,
       networkCheckTimeout: 999999
+    },
+
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000,
+      skipDryRun: true
     }
   },
   compilers: {
